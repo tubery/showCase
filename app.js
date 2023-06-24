fa443a82061a788c287125129d9232ff47ac492f
tubery <pdinh74@gmail.com>
2023-06-24 15:03:32 +0100

    Feat, completed nav functions

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ const hamburgerButton = document.querySelector(".hamburger-button");
 const topBun = document.querySelector(".top-bun");
 const patty = document.querySelector(".patty");
 const bottomBun = document.querySelector(".bottom-bun");
+const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector("#nav-bar");
+const logo = document.querySelector(".logo");
+
+function menuIconToggle() {
+	topBun.classList.toggle("close");
+	patty.classList.toggle("close");
+	bottomBun.classList.toggle("close");
+	navMenu.classList.toggle("open");
+	navBar.classList.toggle("open");
+	logo.classList.toggle("open");
+}
 
 hamburgerButton.addEventListener("click", () => {
-	if (topBun.classList != "top-bun close") {
+	if (hamburgerButton.dataset.state === "open") {
 		// Change to X
+		hamburgerButton.setAttribute("data-state", "close");
+		menuIconToggle();
 	} else {
 		// Return to hamburger
+		hamburgerButton.setAttribute("data-state", "open");
+		menuIconToggle();
 	}
 });
